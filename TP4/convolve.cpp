81c05a7196700243afc2172e316aa1dc6a9672ee
Yoann067 <yoann.louazel@gmail.com>
2020-03-16 13:53:22 +0100

    fin tp4

--- TP4/convolve.cpp
+++ TP4/convolve.cpp
@@ -1,6 +1,7 @@
 //
 // Created by TheWitcher on 02/03/2020.
 //
+#include <string.h>
 #include <unistd.h>
 #include <cstdlib>
 #include <iostream>
@@ -35,10 +36,6 @@ Image<uint8_t> convolve(const Image<uint8_t> &image, const Image<double> &mask){
     int middleMask = dimMaskConv/2;
     int rightBord = dimMaskConv-middleMask;
     int leftBord = middleMask-1;
-    double poidMask = 0;
-    for (int i=0;i<mask.getSize();i++){
-        poidMask+=mask(i);
-    }
     for(int x=0;x<image.getDx();x++) {
         for (int y = 0; y < image.getDy(); y++) {
             index=-1;
@@ -46,12 +43,11 @@ Image<uint8_t> convolve(const Image<uint8_t> &image, const Image<double> &mask){
             for (int xm = x-(middleMask-1); xm < x+rightBord+1; xm++) {
                 for (int ym = y-(middleMask-1); ym < y+rightBord+1; ym++) {
                     index++;
-                    if ( xm>leftBord || xm< image.getDx()-rightBord-1 || ym>leftBord || ym<image.getDy()-rightBord-1){
+                    if ( xm>-1 || xm< image.getDx()-1 || ym>-1 || ym<image.getDy()-1){
                         pixel += image(xm,ym)*mask(index);
                     }
                 }
             }
-            pixel = pixel/poidMask;
             if (pixel<0){
                 image2(x,y) = 0;
             } else if (pixel > 255){
@@ -73,24 +69,27 @@ Image<double> gaussienMask(float sigma){
     double poid=0;
     for(int x = -(n/2); x<(n/2)+1;x++){
         for(int y = -(n/2); y<(n/2)+1;y++){
-            mask(xm) = (1/(2*M_PI*sigma*sigma))*exp(-(x*x+y*y)/2*sigma*sigma);
-            std::cout << " pixel (" << x << ";" << y << ") : " << mask(x,y) << "\n";
+            mask(xm) = exp(-(x*x+y*y)/(2*sigma*sigma));
+            std::cout << " pixel (" << x << ";" << y << ") : " << mask(xm) << "\n";
+            poid += mask(xm);
             xm++;
         }
     }
     for(int i = 0; i< mask.getSize()-1;i++){
-        poid += mask(i);
-    }
-    for(int i = 0; i< mask.getSize()-1;i++){
         mask(i)=mask(i)/poid;
     }
+    poid = 0;
+    for(int i = 0; i< mask.getSize()-1;i++){
+        poid += mask(i);
+    }
+    std::cout << " poid : " << poid << "\n";
     return mask;
 }
 
 
 int main(int argc, const char * argv[]) {
-    if(argc !=4) {
-        std::cout << "Usage : " << argv[0] << " <input.pgm> <output.pgm> <teta>\n";
+    if(argc !=5) {
+        std::cout << "Usage : " << argv[0] << " <input.pgm> <output.pgm> <teta> <option>\n";
         exit(EXIT_FAILURE);
     }
 //    if(argv[3] != '4' && argv[3] != '8'){
@@ -98,23 +97,62 @@ int main(int argc, const char * argv[]) {
 //        exit(EXIT_FAILURE);
 //    }
     std::vector<double> data = {
-            1,1,1,
-            1,1,1,
-            1,1,1
+            1./9,1./9,1./9,
+            1./9,1./9,1./9,
+            1./9,1./9,1./9
     };
     Image<double> mask1(3,3,data);
 
     std::vector<double> data2 = {
+            1./16,2./16,1./16,
+            2./16,4./16,2./16,
+            1./16,2./16,1./16,
+    };
+    Image<double> mask2(3,3,data2);
+    std::vector<double> data3 = {
+            0,-1,0,
+            -1,5,-1,
+            0,-1,0,
+    };
+    Image<double> mask3(3,3,data3);
+    std::vector<double> data4 = {
             -1,-1,-1,
             -1,9,-1,
             -1,-1,-1
     };
-    Image<double> mask2(3,3,data2);
+    Image<double> mask4(3,3,data4);
+    std::vector<double> data5 = {
+            -1,0,1,
+            -2,0,2,
+            -1,0,1
+    };
+    Image<double> mask5(3,3,data5);
+    std::vector<double> data6 = {
+            -1,-2,-1,
+            0,0,0,
+            1,2,1
+    };
+    Image<double> mask6(3,3,data6);
 
     //Image<double> mask3(gaussienMask(atof(argv[3])));
 
     Image<uint8_t> image=readPGM(argv[1]);
     print_info(image);
-    writePGM(convolve(image,gaussienMask(atof(argv[3]))),argv[2]);
+    if(strcmp(argv[4],"ga")==0) {
+        writePGM(convolve(image, gaussienMask(atof(argv[3]))), argv[2]);
+        std::cout << "GaussienMAsk";
+    }else if (strcmp(argv[4],"l1")==0){
+        writePGM(convolve(image, mask1), argv[2]);
+    }else if (strcmp(argv[4],"l2")==0){
+        writePGM(convolve(image, mask2), argv[2]);
+    }else if (strcmp(argv[4],"r3")==0){
+        writePGM(convolve(image, mask3), argv[2]);
+    }else if (strcmp(argv[4],"r4")==0){
+        writePGM(convolve(image, mask4), argv[2]);
+    }else if (strcmp(argv[4],"a5")==0){
+        writePGM(convolve(image, mask5), argv[2]);
+    }else if (strcmp(argv[4],"a6")==0){
+        writePGM(convolve(image, mask6), argv[2]);
+    }
     return 0;
 }
